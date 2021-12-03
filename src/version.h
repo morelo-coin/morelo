0456ba16ce161d362a2d350506945b003eb819e2
Morelo <85998043+morelo-coin@users.noreply.github.com>
2021-12-03 07:44:53 +0100

    redo

---
+++ src/version.h
@@ -0,0 +1,7 @@
+#pragma once
+
+extern const char* const MORELO_VERSION_TAG;
+extern const char* const MORELO_VERSION;
+extern const char* const MORELO_RELEASE_NAME;
+extern const char* const MORELO_VERSION_FULL;
+extern const bool MORELO_VERSION_IS_RELEASE;
