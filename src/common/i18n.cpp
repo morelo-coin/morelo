b71f06ad83d8697a7a2202ba84a8ed07c760deb7
Morelo <85998043+morelo-coin@users.noreply.github.com>
2021-12-09 10:26:30 +0100

    Update i18n.cpp

--- src/common/i18n.cpp
+++ src/common/i18n.cpp
@@ -35,6 +35,7 @@
 #include "file_io_utils.h"
 #include "common/i18n.h"
 #include "translation_files.h"
+#include <algorithm>
 
 #undef MORELO_DEFAULT_LOG_CATEGORY
 #define MORELO_DEFAULT_LOG_CATEGORY "i18n"
